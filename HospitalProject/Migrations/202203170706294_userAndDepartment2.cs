f622949c8a7ef9a5669739403b9a046016a5452b
Tannerliu1993@gmail.com <Tannerliu1993@gmail.com>
2022-03-17 23:25:12 -0400

    department fucntion added

---
+++ HospitalProject/Migrations/202203170706294_userAndDepartment2.cs
@@ -0,0 +1,39 @@
+namespace HospitalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class userAndDepartment2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        DID = c.Int(nullable: false, identity: true),
+                        DName = c.String(),
+                    })
+                .PrimaryKey(t => t.DID);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UID = c.Int(nullable: false, identity: true),
+                        FName = c.String(),
+                        LName = c.String(),
+                        Email = c.String(),
+                        PWD = c.String(),
+                    })
+                .PrimaryKey(t => t.UID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Departments");
+        }
+    }
+}
